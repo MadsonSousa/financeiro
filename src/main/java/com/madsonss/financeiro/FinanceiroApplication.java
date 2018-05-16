4fbf9ab5de94973da648dcbf4f6d7bdaa7f4dc22
MadsonSousa <madson_ss_10@hotmail.com>
2018-05-15 22:43:40 -0300

    Criando instancia da categoria

--- src/main/java/com/madsonss/financeiro/FinanceiroApplication.java
+++ src/main/java/com/madsonss/financeiro/FinanceiroApplication.java
@@ -1,12 +1,31 @@
 package com.madsonss.financeiro;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.madsonss.financeiro.domain.Categoria;
+import com.madsonss.financeiro.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class FinanceiroApplication {
+public class FinanceiroApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceiroApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Despesas");
+		Categoria cat2 = new Categoria(null, "Receitas");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
+	}
+	
 }
